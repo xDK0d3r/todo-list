5e5546cfc51671809b590fe4e7f2aff32686d486
Dhanush K <dhanushkesavaraj.k.dev@gmail.com>
2026-07-05 19:29:19 +0530

    Add Edit and Delete Functionality

--- task_manager.py
+++ task_manager.py
@@ -4,14 +4,41 @@ task_list = []
 
 # Add Task
 def add_task() :
+    # Task add flow
     print("Add Task")
-    adding_task = input("Enter Task :")
-    task_list.append(adding_task)
-    print("task Added Sucessfully")
+    task_add = input("Enter Task :")
+    task_list.append(task_add)
+    print("Task Added Sucessfully")
 
+# Edit Task
+def edit_task() :
+    print("Edit Task")
+    task_id = int(input("Select Task by ID :"))
+    if 0 <= task_id < len(task_list) :
+        task_edit = input("Enter Task :")
+        task_list[task_id] = task_edit
+        print("Task Edited Sucessfully")
+    else :
+        print("invalid Task ID")
+
+# Delete Task
+def delete_task() :
+    print("Delete Task")
+    task_id = int(input("Select Task by ID :"))
+    if 0 <= task_id < len(task_list) :
+        task_list.pop(task_id)
+    else :
+        print("invalid Task ID")
+    
 # View Tasks
 def view_tasks() :
     print ("View All Tasks")
-    print(task_list)
+    print("ID | TASKS")
+    # Enumerate
+    for index, tasks in enumerate(task_list) :
+        print(index," | ",tasks)
+
+
+    
     
 
